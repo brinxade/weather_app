6a06d4a888f086e7dd131d36b5a374ef8d437e53
Gurparsad Bajwa <brinxade@gmail.com>
2023-02-27 00:28:47 -0500

    CORS workaround for API

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,7 @@ import { Utility } from '../app/util';
  
 // In seconds
 export const REALTIME_INTERVAL = 5;
+const API_PATH = `https://${process.env.REACT_APP_API_USER}:${process.env.REACT_APP_API_PASS}@api.meteomatics.com`;
 
 export const DataFormat = {
     temp: {
@@ -10,7 +11,7 @@ export const DataFormat = {
         icon: "fa-temperature-three-quarters",
         visType: "line",
         query: (pos, time) => {
-            let q = `https://api.meteomatics.com/${Utility.getTodaysDateFormatted()}T${time[0]}:00:00.000-05:00--${Utility.getTodaysDateFormatted()}T${time[1]}:00:00.000-05:00:PT30M/t_2m:C/${pos[0]},${pos[1]}/json?model=mix`;
+            let q = `${API_PATH}/${Utility.getTodaysDateFormatted()}T${time[0]}:00:00.000-05:00--${Utility.getTodaysDateFormatted()}T${time[1]}:00:00.000-05:00:PT30M/t_2m:C/${pos[0]},${pos[1]}/json?model=mix`;
             return q;
         }
     },
@@ -20,7 +21,7 @@ export const DataFormat = {
         icon: "fa-droplet",
         visType: "bar",
         query: (pos, time) => {
-            let q = `https://api.meteomatics.com/${Utility.getTodaysDateFormatted()}T${time[0]}:00:00.000-05:00--${Utility.getTodaysDateFormatted()}T${time[1]}:00:00.000-05:00:PT30M/precip_6h:mm/${pos[0]},${pos[1]}/json?model=mix`;
+            let q = `${API_PATH}/${Utility.getTodaysDateFormatted()}T${time[0]}:00:00.000-05:00--${Utility.getTodaysDateFormatted()}T${time[1]}:00:00.000-05:00:PT30M/precip_6h:mm/${pos[0]},${pos[1]}/json?model=mix`;
             return q;
         }
     },
@@ -30,7 +31,7 @@ export const DataFormat = {
         icon: "fa-wind",
         visType: "line",
         query: (pos, time) => {
-            let q = `https://api.meteomatics.com/${Utility.getTodaysDateFormatted()}T${time[0]}:00:00.000-05:00--${Utility.getTodaysDateFormatted()}T${time[1]}:00:00.000-05:00:PT30M/wind_speed_FL10:kmh/${pos[0]},${pos[1]}/json?model=mix`;
+            let q = `${API_PATH}/${Utility.getTodaysDateFormatted()}T${time[0]}:00:00.000-05:00--${Utility.getTodaysDateFormatted()}T${time[1]}:00:00.000-05:00:PT30M/wind_speed_FL10:kmh/${pos[0]},${pos[1]}/json?model=mix`;
             return q;
         }
     }
